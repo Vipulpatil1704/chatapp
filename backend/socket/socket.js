08022a773e31ac0628ca87758f1a3c0ef14a31fb
Vipulpatil1704 <vipul.d.patil1704@gmail.com>
2024-12-04 02:39:59 +0530

    c15

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -11,6 +11,7 @@ const io=new Server(server,{
         credentials:true
     }
 })
+//websocket
 const userSocketMap={};
 // const undeliveredMsg={};
 // export const getUndeliveredMsg=(msg,receiverId)=>{
